b55fc45fdca86f02f18247e1a03cfefcf94bdd1a
Antonio Aloisio <antonio.aloisio@nokia.com>
2011-02-02 14:23:23 +0200

    Antico debug messages are printed out only if  ANTICO_DEBUG environment variable is set

--- qde/wm/main.cpp
+++ qde/wm/main.cpp
@@ -2,12 +2,37 @@
 #include "defs.h"
 #include "adx.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
 extern Adx *deluxe;
+static bool debugMode = false;
+
+void anticoMessageOutput(QtMsgType type, const char *msg)
+{
+    switch (type) {
+    case QtDebugMsg:
+	if (!debugMode) return;
+	fprintf(stderr, "Debug: %s\n", msg);
+	break;
+    case QtWarningMsg:
+	fprintf(stderr, "Warning: %s\n", msg);
+	break;
+    case QtCriticalMsg:
+	fprintf(stderr, "Critical: %s\n", msg);
+	break;
+    case QtFatalMsg:
+	fprintf(stderr, "Fatal: %s\n", msg);
+	abort();
+    }
+}
 
 int main(int argc, char **argv)
 {
-        Adx a(argc, argv);
+	debugMode = !qgetenv("ANTICO_DEBUG").isEmpty();
 
+        Adx a(argc, argv);
+	qInstallMsgHandler(anticoMessageOutput);
         deluxe = &a;
         //(void) QApplication::desktop();
 
